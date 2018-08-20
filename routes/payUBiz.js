b2c01dc7d75091351e3a5067e8c32f1bb8cac834
Bismah Qadri <bismahqadri@gmail.com>
2018-08-20 13:44:22 +0530

    integrating payu biz success route

--- routes/payUBiz.js
+++ routes/payUBiz.js
@@ -25,5 +25,117 @@ router.post('/', function(req, res, next) {
     res.send(data)
 
 });
+router.post('/success', function (req, res) {
+    const body = req.body
+    console.log('======================success',body)
+    const status = body.status
+    const firstname = body.firstname
+    const amount = body.amount
+    const txnid = body.txnid
+    const hash = body.hash
+    const key = body.key
+    const productinfo = body.productinfo
+    const email = body.email
+    const udf1 = body.udf1
+    const udf2 = body.udf2
+    const udf3 = body.udf3
+    const udf4 = body.udf4
+    const udf5 = body.udf5
+    const additionalCharges = body.additionalCharges
+    const mode = body.mode
+
+    const salt = udf1 && udf1.toUpperCase() === 'PRODUCTION' ? payU.production.salt : payU.staging.salt
+
+    let params
+    if (additionalCharges) {
+        params = checkNull(additionalCharges) + '|' + checkNull(salt) + '|' + checkNull(status) + '||||||' + checkNull(udf5) +
+            '|' + checkNull(udf4) + '|' + checkNull(udf3) + '|' + checkNull(udf2) + '|' + checkNull(udf1) + '|' +
+            checkNull(email) + '|' + checkNull(firstname) + '|' + checkNull(productinfo) + '|' + checkNull(amount) +
+            '|' + checkNull(txnid) + '|' + checkNull(key)
+    } else {
+        params = checkNull(salt) + '|' + checkNull(status) + '||||||' + checkNull(udf5) + '|' + checkNull(udf4) +
+            '|' + checkNull(udf3) + '|' + checkNull(udf2) + '|' + checkNull(udf1) + '|' + checkNull(email) + '|' +
+            checkNull(firstname) + '|' + checkNull(productinfo) + '|' + checkNull(amount) + '|' + checkNull(txnid) +
+            '|' + checkNull(key)
+    }
+    const hash512 = crypto.createHash('sha512')
+    hash512.update(params)
+    let reverseHash =  hash512.digest('hex')
+    if (reverseHash === hash) {
+        // save payment transactions in DB
+        res.render('status', {
+            heading: 'Success',
+            message: 'Successfully Done',
+            class: 'fa fa-times-circle text-danger'
+        })
+    } else {
+        res.render('status', {
+            heading: 'Error',
+            message: 'Invalid Transaction. Please try again',
+            class: 'fa fa-times-circle text-danger'
+        })
+        console.log('==================PayU PAYMENT=================')
+        console.log('Invalid hash')
+        console.log('===================ENDS HERE====================')
+    }
+})
+
+router.post('/failure', function (req, res) {
+    const body = req.body
+    console.log('==========failure',body)
+    const status = body.status
+    const firstname = body.firstname
+    const amount = body.amount
+    const txnid = body.txnid
+    const hash = body.hash
+    const key = body.key
+    const productinfo = body.productinfo
+    const email = body.email
+    const udf1 = body.udf1
+    const udf2 = body.udf2
+    const udf3 = body.udf3
+    const udf4 = body.udf4
+    const udf5 = body.udf5
+    const additionalCharges = body.additionalCharges
+
+    const salt = udf1 && udf1.toUpperCase() === 'PRODUCTION' ? payU.production.salt : payU.staging.salt
+
+    let params
+    if (additionalCharges) {
+        params = checkNull(additionalCharges) + '|' + checkNull(salt) + '|' + checkNull(status) + '||||||' + checkNull(udf5) +
+            '|' + checkNull(udf4) + '|' + checkNull(udf3) + '|' + checkNull(udf2) + '|' + checkNull(udf1) + '|' +
+            checkNull(email) + '|' + checkNull(firstname) + '|' + checkNull(productinfo) + '|' + checkNull(amount) +
+            '|' + checkNull(txnid) + '|' + checkNull(key)
+    } else {
+        params = checkNull(salt) + '|' + checkNull(status) + '||||||' + checkNull(udf5) + '|' + checkNull(udf4) +
+            '|' + checkNull(udf3) + '|' + checkNull(udf2) + '|' + checkNull(udf1) + '|' + checkNull(email) + '|' +
+            checkNull(firstname) + '|' + checkNull(productinfo) + '|' + checkNull(amount) + '|' + checkNull(txnid) +
+            '|' + checkNull(key)
+    }
+
+    const hash512 = crypto.createHash('sha512')
+    hash512.update(params)
+    let reverseHash =  hash512.digest('hex')
+    if (reverseHash === hash) {
+        res.render('status', {
+            heading: 'Error',
+            message: 'Transaction Failed. Your payment status is ' + status,
+            class: 'fa fa-times-circle text-danger'
+        })
+
+        console.log('============PayU PAYMENT FAILURE=============')
+        console.log(body)
+        console.log('===================ENDS HERE====================')
+    } else {
+        res.render('status', {
+            heading: 'Error',
+            message: 'Invalid Transaction. Please try again',
+            class: 'fa fa-times-circle text-danger'
+        })
+        console.log('==================PayU PAYMENT=================')
+        console.log('Invalid hash')
+        console.log('===================ENDS HERE====================')
+    }
+})
 
 module.exports = router;
